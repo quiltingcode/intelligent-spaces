a652085da4afaba81754431ad4dc4b700756ff89
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-14 19:14:35 +0000

    format issue time data

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 * I referred back to the Cave of Query portfolio project that I created, when joining two columns of data together; the issue date and issue time columns
 
-* 
+* [Inserting Characters](https://stackoverflow.com/questions/71701814/how-to-insert-a-character-into-a-string-at-a-certain-index)
 
 
 
--- run.py
+++ run.py
@@ -17,6 +17,8 @@ SHEET = GSPREAD_CLIENT.open('intelligent-spaces-test')
 sheet = GSPREAD_CLIENT.open('intelligent-spaces-test').sheet1
 data = sheet.get_all_records()
 
+""" Challenge 1 - Combine the Issue Date and Issue time into a single Issue Date Time value """
+
 
 """ Remove 00:00:00 time from issue date column """
 issue_date_col = list(sheet.col_values(2))
@@ -30,12 +32,31 @@ for row in issue_date_col:
 
 issue_time_col = list(sheet.col_values(3))
 
+formatted_times = []
+
+
+def make_four_digits(data):
+
+    if len(data) <= 3:
+        new_time = str(data.zfill(4))
+        formatted_times.append(new_time)
+    else:
+        formatted_times.append(data)
+
+
+for row in issue_time_col:
+    make_four_digits(row)
+
+
+formatted_times = [f"{el[:2]}:{el[2:]}" for el in formatted_times]
+
+
 """ Concatenate the two columns together """
 
-combined_columns = {spliced_date[i] + issue_time_col[i] for i in range(len(spliced_date))}
+combined_columns = {spliced_date[i] + formatted_times[i] for i in range(len(spliced_date))}
 print(combined_columns)
 
 
-
+""" Challenge 2 - Convert the plate expiry value into a date value with the day as the last day of the month """
 
 
